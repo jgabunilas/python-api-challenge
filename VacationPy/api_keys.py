35b9bda593a00eaa26ac6117fb657667f59b4876
unknown <jgabunil@ucla.edu>
2020-07-09 09:43:16 -0700

    Update 07092020

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "fedd0dcde979afef550c7c347987149e"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAH33585yZlTCQnS5UWx9o3f3PWDT8Ua7c"
